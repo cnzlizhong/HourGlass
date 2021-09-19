ad7a1c57e7430e8be184f393b6b0608eaf7d027e
Lizhong Zhang <cnzlizhong@gmail.com>
2021-09-20 00:33:11 +1200

    Create DB context

--- MinGlass.API/MinGlass.API/Startup.cs
+++ MinGlass.API/MinGlass.API/Startup.cs
@@ -2,15 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MinGlass.Repository.Context;
 
 namespace MinGlass.API
 {
@@ -26,7 +24,7 @@ namespace MinGlass.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<AppContext>(opt => opt.UseNpgsql(Configuration.GetConnectionString("Admin")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
---
+++ MinGlass.API/MinGlass.Repository.Context/AppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinGlass.Models;
+
+namespace MinGlass.Repository.Context
+{
+    public class AppContext: DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
